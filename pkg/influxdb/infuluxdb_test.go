d1b4180efab64fdb4f95578f941aeccccde39f0a
super2399 <xjc23456789@163.com>
2020-02-08 00:56:12 +0800

    feat:新增不同类型数据单元测试添加数据脚本,mqtt数据校验

--- pkg/influxdb/infuluxdb_test.go
+++ pkg/influxdb/infuluxdb_test.go
@@ -47,16 +47,33 @@ func Test_AddService(t *testing.T) {
 	c := NewInfluxdbClient()
 	InfluxClient = c
 	deviceId := "1XKCqOEZn90hsUtWFafPWhfqu1A"
-	property := "LightAdjustLevel"
-	value := 66
-	params := map[string]interface{}{property: value}
-	paramsStr, _ := json.Marshal(params)
-	service := map[string]interface{}{"identifier": "set", "service_name": "set", "params": string(paramsStr), "msg_id": 2}
-	AddPointToService(deviceId, service)
+	property1 := "LightAdjustLevel"
+	value1 := 66
+	params1 := map[string]interface{}{property1: value1}
+	paramsStr1, _ := json.Marshal(params1)
+	service1 := map[string]interface{}{
+		"identifier": "set",
+		"service_name": "set",
+		"params": string(paramsStr1),
+		"msg_id": 2,
+	}
+	AddPointToService(deviceId, service1)
+	time.Sleep(1*time.Second)
+	property2 := "TimeReset"
+	value2 := "test"
+	params2 := map[string]interface{}{property2: value2}
+	paramsStr2, _ := json.Marshal(params2)
+	service2 := map[string]interface{}{
+		"identifier": "TimeReset",
+		"service_name": "TimeReset",
+		"params": string(paramsStr2),
+		"msg_id": 2,
+	}
+	AddPointToService(deviceId, service2)
 	end := time.Now().Unix()
 	m, _ := time.ParseDuration("-10m")
 	start := time.Now().Add(m).Unix()
-	serviceInfo := GetDeviceServiceInfoFromService(deviceId, "set", start, end, false)
+	serviceInfo := GetDeviceServiceInfoFromService(deviceId, "all", start, end, false)
 	if serviceInfo == nil { //try a unit test on function
 		t.Error("时许数据库写入设备服务测试不通过") // 如果不是如预期的那么就报错
 	} else {
@@ -69,18 +86,44 @@ func Test_AddEvent(t *testing.T) {
 	c := NewInfluxdbClient()
 	InfluxClient = c
 	deviceId := "1XKCqOEZn90hsUtWFafPWhfqu1A"
-	property := "alarmType"
-	value := 1
-	params := map[string]interface{}{property: value}
-	paramsStr, _ := json.Marshal(params)
-	service := map[string]interface{}{
+	property1 := "ErrorCode"
+	value1 := 0
+	params1 := map[string]interface{}{property1: value1}
+	paramsStr1, _ := json.Marshal(params1)
+	event1 := map[string]interface{}{
+		"identifier": "Error",
+		"event_name": "故障上报",
+		"event_type": "info",
+		"outputData": string(paramsStr1),
+		"msg_id":     1,
+	}
+	AddPointToEvent(deviceId, event1)
+	time.Sleep(1*time.Second)
+	property2 := "alarmType"
+	value2 := 1
+	params2 := map[string]interface{}{property2: value2}
+	paramsStr2, _ := json.Marshal(params2)
+	event2 := map[string]interface{}{
 		"identifier": "alarmEvent",
 		"event_name": "报警事件",
 		"event_type": "alert",
-		"outputData": string(paramsStr),
+		"outputData": string(paramsStr2),
 		"msg_id":     1,
 	}
-	AddPointToEvent(deviceId, service)
+	AddPointToEvent(deviceId, event2)
+	time.Sleep(1*time.Second)
+	property3 := "Error"
+	value3 := 1
+	params3 := map[string]interface{}{property3: value3}
+	paramsStr3, _ := json.Marshal(params3)
+	event3 := map[string]interface{}{
+		"identifier": "CommunicationFault",
+		"event_name": "通讯故障",
+		"event_type": "error",
+		"outputData": string(paramsStr3),
+		"msg_id":     1,
+	}
+	AddPointToEvent(deviceId, event3)
 	end := time.Now().Unix()
 	m, _ := time.ParseDuration("-10m")
 	start := time.Now().Add(m).Unix()
