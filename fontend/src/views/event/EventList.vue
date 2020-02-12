30c08f10dddc02fb95348a376926a254d30e08ca
liujingxian <18273427041@139.com>
2020-02-12 18:24:34 +0800

    fix:事件管理接口联调修复

--- fontend/src/views/event/EventList.vue
+++ fontend/src/views/event/EventList.vue
@@ -27,6 +27,16 @@
                 :value="item.value">
                 </el-option>
           </el-select>
+           <el-date-picker v-if="time === '4'"
+                    v-model="timeArr"
+                    @change="changeTimeArr"
+                    type="datetimerange"
+                    range-separator="至"
+                    align="right"
+                    start-placeholder="起始日期"
+                    end-placeholder="结束日期"
+                    size="medium">
+           </el-date-picker>
     </el-row> 
     
     <el-table
@@ -46,7 +56,7 @@
             width="200">
         </el-table-column>
         <el-table-column
-            :label="type === 'event' ? '事件名称' : '服务名称'"  >
+            :label="type === 'event' ? '事件名称' : '服务名称'"   width="200">
             <template slot-scope="scope">{{ scope.row.name  }}</template>
         </el-table-column>
             
@@ -56,14 +66,14 @@
         </el-table-column>
         <el-table-column
             label="输入参数"  v-else >
-            <template slot-scope="scope">{{ scope.row.input_data  }}</template>
+            <template slot-scope="scope">{{ scope.row.input_data }}</template>
         </el-table-column>
         <el-table-column
             label="输出参数"  >
-            <template slot-scope="scope">{{ scope.row.output_data  }}</template>
+            <template slot-scope="scope">{{ scope.row.output_data }}</template>
         </el-table-column>
     </el-table>
-    <el-button @click="loadMore" v-if="isBtnShow">加载更多</el-button>
+    <el-button @click="init" v-if="isBtnShow">加载更多</el-button>
   
   </div>
 </template>
@@ -99,7 +109,7 @@
     data() {
       return {
         tableData: [],
-        eventTypes: [{label:'信息',value:'info'},{label:'告警',value:'alert'},{label:'故障',value:'error'}],
+        eventTypes: [{label:'全部类型',value:''},{label:'信息',value:'info'},{label:'告警',value:'alert'},{label:'故障',value:'error'}],
         identifier:'',
         eventType:'',
         times:[{label:'1小时',value:'1'},{label:'24小时',value:'2'},{label:'7天',value:'3'},{label:'自定义',value:'4'}],
@@ -107,7 +117,9 @@
         currentPage:1,
         startTime:0,
         endTime:0,
-        isBtnShow:false
+        timeArr:[],
+        isBtnShow:false,
+        next_time:0
       }
     },
  
@@ -126,44 +138,45 @@
           }
       },
        getDiviceList(){
-           this.$API_IOT.getEventList(this.deviceId,this.currentPage,this.startTime,this.endTime,this.eventType,this.identifier).then((res) => {
-             if(this.currentPage === 1){
-                this.tableData = res.data.data
+           this.$API_IOT.getEventList(this.deviceId,this.currentPage,this.startTime,this.next_time === 0 ? this.endTime :this.next_time ,this.eventType,this.identifier).then((res) => {
+            
+             if(res.data.data.data_list != null){
+               this.tableData = this.tableData.concat(res.data.data.data_list)
              }else{
-               this.tableData = this.tableData.concat(res.data.data)
+               this.tableData = []
              }
-             if(res.data.data){
-               this.isBtnShow = true
+             if(res.data.data.next_time !== 0 ){
+               this.isBtnShow = true     
              }else{
                this.isBtnShow = false
              }
-                
+              this.next_time = res.data.data.next_time    
+ 
             })
            
        },
        getServiceList(){
-           this.$API_IOT.getServiceList(this.deviceId,this.currentPage,this.startTime,this.endTime,this.identifier).then((res) => {
-               if(this.currentPage === 1){
-                this.tableData = res.data.data
+           this.$API_IOT.getServiceList(this.deviceId,this.currentPage,this.startTime,this.next_time === 0 ? this.endTime :this.next_time ,this.identifier).then((res) => {
+                if(res.data.data.data_list != null){ 
+                  this.tableData = this.tableData.concat(res.data.data.data_list)
                 }else{
-                  this.tableData = this.tableData.concat(res.data.data)
+                  this.tableData = []
                 }
-                if(res.data.data){
-                  this.isBtnShow = true
+                if(res.data.data.next_time !== 0 ){
+                  this.isBtnShow = true     
                 }else{
                   this.isBtnShow = false
                 }
+
+                this.next_time = res.data.data.next_time    
+
             })
            
        },
-       loadMore(){
-         this.currentPage += 1
-         this.init() 
-       },
+      
        changeTime(value){
            let nowDate  = new Date()
            this.endTime = nowDate.getTime().toString().substr(0,10)    
-
             if(value === "1"){//1小时
                 this.startTime = nowDate.setHours(nowDate.getHours() -1).toString().substr(0,10)
             }else if(value === "2"){//24小时
@@ -171,15 +184,28 @@
             }else if(value === "3"){//7天
                 this.startTime = nowDate.setDate(nowDate.getDate() - 7).toString().substr(0,10)
             }else {//自定义
-                
+              //1-清空
+              this.timeArr = []
+              //2-赋值 
+              let end = new Date().getTime()
+              let start  = new Date(new Date().setHours(nowDate.getHours() -1))
+              this.timeArr = [start,end]
+              this.startTime = start.getTime().toString().substr(0,10)
+              this.endTime = end.toString().substr(0,10)
             }
-            this.currentPage = 1
+            this.init()
+       },
+       changeTimeArr(value){
+            this.startTime = value[0].getTime().toString().substr(0,10)
+            this.endTime = value[1].getTime().toString().substr(0,10)
+            this.next_time = 0 
             this.init()
        },
 
        changeEventType(value ){
          this.eventType = value 
-          this.init()
+         this.next_time = 0 
+         this.init()
        }
       
     }
