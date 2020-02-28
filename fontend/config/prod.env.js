6f0ce0bf96643047053fac7b4e8aeb87006f4117
liujingxian <18273427041@139.com>
2020-02-28 15:12:52 +0800

    fix:修改配置文件环境地址

--- fontend/config/prod.env.js
+++ fontend/config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_IOT:'"http://139.199.89.93:9898/iot/api/v1"',
+  API_IOT:'"http://118.24.8.219:8080/iot/api/v1"',
   API_SOTA:'"http://10.10.6.15:8000/sota/api/v1"'
 }
