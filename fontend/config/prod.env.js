0f2d3a957324a62ca535436f432dd17c1f709b78
liujingxian <18273427041@139.com>
2020-03-07 15:32:41 +0800

    fit:修改配置文件服务器端口号

--- fontend/config/prod.env.js
+++ fontend/config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_IOT:'"http://118.24.8.219:8080/iot/api/v1"',
-  CLOUD_IOT:'"http://118.24.8.219:8080/v-cloud/api/v1"',
+  API_IOT:'"http://118.24.8.219:9898/iot/api/v1"',
+  CLOUD_IOT:'"http://118.24.8.219:9898/v-cloud/api/v1"',
   API_SOTA:'"http://10.10.6.15:8000/sota/api/v1"'
 }
